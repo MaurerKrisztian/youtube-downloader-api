b291a40db75b16f30e7bcae11e9971e9a4ef54e9
krisztian.maurer <krisztian.maurer@metrix.co.hu>
2022-07-02 14:17:56 +0200

    fix(app): tmp download link

--- src/downloader-sockets/handle-request.ts
+++ src/downloader-sockets/handle-request.ts
@@ -17,7 +17,7 @@ export class HandleRequest {
             })
             YtDownloaderWrapper.DownloadEvent.on(YtDownloaderWrapper.EventNames.DONE, (data) => {
                 console.log("done", data)
-                socket.emit('download-done', {link: `http://localhost:3000/${data.id}/${data.id}`, pid: data.pid} )
+                socket.emit('download-done', {link: `https://test.mytaskplan.me/${data.id}/${data.id}`, pid: data.pid} ) // todo from env
             })
             YtDownloaderWrapper.DownloadEvent.on(YtDownloaderWrapper.EventNames.ERROR, (data) => {
                 console.log("error", data)
