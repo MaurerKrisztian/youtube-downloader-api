051125df40657be87d26cf809ebf4a8699f1508b
krisztian.maurer <krisztian.maurer@metrix.co.hu>
2022-07-17 20:34:05 +0200

    fix(deploy): railway banned dep detection test

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,21 @@
 import {MyLogger} from "./Logger";
 import {Server} from "./Server";
+import path from "path";
 require('dotenv/config')
 
 MyLogger.logger.level = "trace"
 
 const server = Server.createApp();
+
+
+// export const BIN_PATH = path.join(__dirname, 'railway-pls')
+// console.log("bin-path", BIN_PATH)
+// async function getYtDlpBin() {
+//     const YTDlpWrap = require('yt-dlp-wrap').default;
+//     await YTDlpWrap.downloadFromGithub(BIN_PATH);
+// }
+
+// getYtDlpBin().catch(err => console.log(err))
 
 const PORT = 3088;
 server.listen(PORT, () => {
